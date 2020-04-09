2cc1bd9b07506d4c707c28f56216da0b8d6e7899
dual <dual@dr.h>
2020-04-09 15:34:24 +0800

    version 1.0.0

--- extension.js
+++ extension.js
@@ -1,35 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zsh" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('zsh.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zsh!');
-	});
-
-	context.subscriptions.push(disposable);
+function detect() {
+	if (vscode.window.activeTextEditor.document.languageId != 'plaintext') {
+		return
+	}
+	const line = vscode.window.activeTextEditor.document.lineAt(0).text
+	if (line && line.match(/^#compdef|^#autoload/)) {
+		vscode.languages
+			.setTextDocumentLanguage(vscode.window.activeTextEditor.document, "shellscript")
+			.catch(err => {
+				console.error(err)
+			})
+	}
 }
+
+function activate() {
+	vscode.window.onDidChangeVisibleTextEditors(() => {
+		detect()
+	})
+	detect()
+}
+
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
